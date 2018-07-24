edaf706f437fd58abec23ed8d0816d704202bcaa
Jungwon Seo <wsbtgt@gmail.com>
2018-07-24 12:46:42 +0200

    update

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template,request
+from main import MLprocessing
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def main():
+
+    question=request.args.get('q', "Does Korean stil eat dog?")
+    return MLprocessing(question)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9000)
