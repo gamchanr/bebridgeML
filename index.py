aec99546169541a555681e80318c579b14920b29
Jungwon Seo <wsbtgt@gmail.com>
2018-08-01 14:26:09 +0200

    update

--- index.py
+++ index.py
@@ -87,6 +87,7 @@ if __name__ == "__main__":
         time.sleep(5)
         print("Start")
         es.indices.create(index=INDEX_NAME, body=INDEX_SETTINGS)
+        time.sleep(5)
         questions = mongo.db.questions.find({'answer_count': { '$gte': 1 }})
         for question in questions :
             # print(question['text'])
